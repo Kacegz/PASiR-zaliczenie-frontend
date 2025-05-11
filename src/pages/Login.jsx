79fbee576493883737343eb0e27c3a7c4c43ea53
Kacegz <90635502+Kacegz@users.noreply.github.com>
2025-05-11 19:50:57 +0200

    update readme

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,7 +26,6 @@ const Login = () => {
       ...prev,
       [name]: value,
     }))
-    // Clear error when user starts typing
     if (errors[name]) {
       setErrors((prev) => ({
         ...prev,
